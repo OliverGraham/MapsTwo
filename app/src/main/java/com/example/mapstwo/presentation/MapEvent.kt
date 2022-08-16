5043c39653d8f49ba9c7b6bb2ecad70cb1266f0e
Oliver <grahamol@oregonstate.edu>
2022-08-16 11:43:05 -0700

    Add repo dependency to viewModel; new map events

--- app/src/main/java/com/example/mapstwo/presentation/MapEvent.kt
+++ app/src/main/java/com/example/mapstwo/presentation/MapEvent.kt
@@ -1,5 +1,12 @@
 package com.example.mapstwo.presentation
 
+import com.example.mapstwo.domain.model.ParkingSpot
+import com.google.android.gms.maps.model.LatLng
+
 sealed class MapEvent {
     object ToggleFalloutMap: MapEvent()
+
+    // These events interact with the database
+    data class OnMapLongClick(val latLng: LatLng): MapEvent()
+    data class OnInfoWindowLongClick(val spot: ParkingSpot): MapEvent()
 }
